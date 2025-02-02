3a2185546625824119ee43a2c0574db1094f2469
bmojek <bartekmojek@gmail.com>
2025-02-02 19:30:16 +0100

    maxProfit done

---
+++ maxProfit/app.ts
@@ -0,0 +1,13 @@
+function maxProfit(prices: number[]): number {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
